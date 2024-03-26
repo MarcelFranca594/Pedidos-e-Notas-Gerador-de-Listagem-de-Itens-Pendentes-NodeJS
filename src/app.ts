76a099c61c8008cee973934b93f5f53e25bfa03f
marcelfranca594@gmail.com <marcelfranca594@gmail.com>
2024-03-26 15:18:27 -0400

    Criando rotas

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
-import fastify from "fastify";
+import { notesRoutes } from './routers/notes.routes'
+import { ordersRoutes  } from './routers/orders.routes'
 
 export const app = fastify()
+
+app.register(cookie)
+
+app.register(notesRoutes, { prefix: 'notes' })
+app.register(ordersRoutes,{ prefix: 'orders' })
